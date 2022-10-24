ea8c7efb4b3f174bb263248bd77afc272ff883c5
slthurman01 <slthurman01@gmail.com>
2022-10-24 18:52:05 -0500

    Adds tailwind functionality.

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './index.html',
+    './assets/js/script.js'
+],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+// To begin running tailwind:
+//
+// npx tailwindcss -i ./assets/css/style.css -o ./assets/CSS/tailwind.css --watch
+
