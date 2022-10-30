53b75cc7690076cd8e985c49594e7bea6e55c4f6
User <kyle_albright@hotmail.com>
2022-10-30 14:04:30 -0500

    added the posters from API data, and styled the modal

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,12 +4,12 @@ var apiKeyYt = "AIzaSyCxcfePxYwFPi4vIK2xuiRgLTDvmgYCDrY";
 var currentDay = moment().format("DD-MM-YYYY");
 var globalVideoId;
 
-var zeroStarEl = document.getElementById("zeroStar")
-var oneStarEl = document.getElementById("oneStar")
-var twoStarEl = document.getElementById("twoStar")
-var threeStarEl = document.getElementById("threeStar")
-var fourStarEl = document.getElementById("fourStar")
-var fiveStarEl = document.getElementById("fiveStar")
+var zeroStarEl = document.getElementById("zeroStar");
+var oneStarEl = document.getElementById("oneStar");
+var twoStarEl = document.getElementById("twoStar");
+var threeStarEl = document.getElementById("threeStar");
+var fourStarEl = document.getElementById("fourStar");
+var fiveStarEl = document.getElementById("fiveStar");
 
 zeroStarEl.style.display = "none";
 oneStarEl.style.display = "none";
@@ -25,7 +25,6 @@ fiveStarEl.style.display = "none";
 function handleDropdown() {
   document.getElementById("myDropdown").classList.toggle("show");
 }
-
 
 window.onclick = function (event) {
   if (!event.target.matches(".dropbtn")) {
@@ -79,67 +78,88 @@ window.onclick = function (event) {
       var movieTitle = $("#movieTitle");
       var releaseDate = $("#releaseDate");
       var movieOverview = $("#movieOverview");
-    
 
-      var posterBaseUrl = "https://image.tmdb.org/t/p/original/"
-      var posterSpecificUrl = (data.results[0].poster_path)
-      
+      var posterBaseUrl = "https://image.tmdb.org/t/p/original/";
+      var posterSpecificUrl = data.results[0].poster_path;
+
       var img = document.createElement("img");
-      img.src= posterBaseUrl+posterSpecificUrl;
-      var src= document.getElementById("moviePoster");
-      src.appendChild(img); 
+      img.src = posterBaseUrl + posterSpecificUrl;
+      var src = document.getElementById("moviePoster");
+      src.appendChild(img);
 
-        // function posterUrl()
-        // posterBaseUrl+posterSpecificUrl
-      // moviePoster.src(posterBaseUrl+posterSpecificUrl).attr("src");
       movieTitle.text(data.results[0].title);
       releaseDate.text(data.results[0].release_date);
-      movieRating.text(data.results[0].vote_average / 2);
       movieOverview.text(data.results[0].overview);
 
       //Setting local storage with an array from the TMDB fetch call
-      savedMovie = data.results[0].title;
-      var movieHistArr = JSON.parse(localStorage.getItem("savedMovie"));
-      if (!movieHistArr) {
-        movieHistArr = [];
-      }
-      if (!movieHistArr.includes(savedMovie)) {
-        movieHistArr.push(savedMovie);
-      }
-      localStorage.setItem("savedMovie", JSON.stringify(movieHistArr));
-      console.log(movieHistArr);
+
+      $("#saveButton").on("click", function () {
+        savedMovie = data.results[0].title;
+        var movieHistArr = JSON.parse(localStorage.getItem("savedMovie"));
+        if (!movieHistArr) {
+          movieHistArr = [];
+        }
+        if (!movieHistArr.includes(savedMovie)) {
+          movieHistArr.push(savedMovie);
+        }
+        localStorage.setItem("savedMovie", JSON.stringify(movieHistArr));
+        console.log(movieHistArr);
+
+        // _______________________________________________________________________________
+
+        for (var i = 0; i < movieHistArr.length; i++) {
+          var storedMovieEl = $(
+            '<button class= "bg-slate-800 hover:bg-red-500 w-full px-4 py-2 text-reg text-slate-400 rounded">'
+          );
+          var storedMovie = storedMovieEl
+            .text(movieHistArr[i])
+            .val(movieHistArr[i]);
+          storedMovie.click(function () {
+            console.log($(this).val());
+            handleSubmit($(this).val());
+          });
+          $("#myList").append(storedMovie);
+        }
+      });
 
       // _______________________________________________________________________________
       // _______________________________________________________________________________
 
       //Setting a visible rating through a five star rating system
       function assignStars() {
-        if ((data.results[0].vote_average / 2) < 1) {
+        if (data.results[0].vote_average / 2 < 1) {
           zeroStarEl.style.display = "block";
-        }
-        else if ((data.results[0].vote_average / 2) >= 1 && (data.results[0].vote_average / 2) < 2) {
+        } else if (
+          data.results[0].vote_average / 2 >= 1 &&
+          data.results[0].vote_average / 2 < 2
+        ) {
           oneStarEl.style.display = "block";
-        }
-        else if ((data.results[0].vote_average / 2) >= 2 && (data.results[0].vote_average / 2) < 3) {
+        } else if (
+          data.results[0].vote_average / 2 >= 2 &&
+          data.results[0].vote_average / 2 < 3
+        ) {
           twoStarEl.style.display = "block";
-        }
-        else if ((data.results[0].vote_average / 2) >= 3 && (data.results[0].vote_average / 2) < 4) {
+        } else if (
+          data.results[0].vote_average / 2 >= 3 &&
+          data.results[0].vote_average / 2 < 4
+        ) {
           threeStarEl.style.display = "block";
-        }
-
-        else if ((data.results[0].vote_average / 2) >= 4 && (data.results[0].vote_average / 2) < 5) {
+        } else if (
+          data.results[0].vote_average / 2 >= 4 &&
+          data.results[0].vote_average / 2 < 5
+        ) {
           fourStarEl.style.display = "block";
         } else {
           fiveStarEl.style.display = "block";
         }
       }
-      assignStars()
+      assignStars();
       // _______________________________________________________________________________
       // _______________________________________________________________________________
 
       //YouTube API call for Trailer access, pulling title from TMDB fecth
       var youtubeSearch = data.results[0].title;
-      
+
       fetch(
         `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=1&q=${youtubeSearch}trailer&key=${apiKeyYt}`
       )
@@ -168,12 +188,12 @@ firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 // 3. This function creates an <iframe> (and YouTube player)
 //    after the API code downloads.
 
-function onYouTubeIframeAPIReady() { }
+function onYouTubeIframeAPIReady() {}
 function getGlobalID() {
   // var player;
   player = new YT.Player("player", {
     height: "300",
-    width: "560",
+    width: "460",
     videoId: globalVideoId,
     playerVars: {
       playsinline: 1,
@@ -196,12 +216,8 @@ function onPlayerReady(event) {
 var done = false;
 function onPlayerStateChange(event) {
   if (event.data == YT.PlayerState.PLAYING && !done) {
-    setTimeout(stopVideo, 6000);
     done = true;
   }
-}
-function stopVideo() {
-  player.stopVideo();
 }
 
 // My List modal
@@ -218,6 +234,6 @@ myListBtn.addEventListener("click", openModal);
 
 var closeModal = function () {
   modal.classList.add("hidden");
-}
+};
 
-returnToResults.addEventListener("click", closeModal)
+returnToResults.addEventListener("click", closeModal);
